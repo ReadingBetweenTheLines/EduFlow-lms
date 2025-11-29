3f140c379b6eb4416e76e1a95d41852ecdd6c391
Bayo <bayohafidzi1998@gmail.com>
2025-11-29 10:08:53 +0700

    fifth attempt

--- eduflow/src/main.jsx
+++ eduflow/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' // We import the default export as 'App'
 import './index.css'
+import EduFlowApp from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> {/* We render 'App', not 'EduFlowApp' */}
+    <EduFlowApp /> 
   </React.StrictMode>,
 )
